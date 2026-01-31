24098756e27179b76f4ea8c6d437c6c108ccc458
MmmarRTHa <m27842n@outlook.com>
2026-01-31 15:12:20 -0800

    - update: feature/chapter_2 - load related resource data for showing artists's albums

--- lib/tunez_web/live/artists/show_live.ex
+++ lib/tunez_web/live/artists/show_live.ex
@@ -8,7 +8,7 @@ defmodule TunezWeb.Artists.ShowLive do
   end
 
   def handle_params(%{"id" => artist_id}, _url, socket) do
-    artist = Tunez.Music.get_artist_by_id!(artist_id)
+    artist = Tunez.Music.get_artist_by_id!(artist_id, load: [:albums])
 
     albums = [
       %{
@@ -58,7 +58,7 @@ defmodule TunezWeb.Artists.ShowLive do
       </.button_link>
 
       <ul class="mt-10 space-y-6 md:space-y-10">
-        <li :for={album <- @albums}>
+        <li :for={album <- @artist.albums}>
           <.album_details album={album} />
         </li>
       </ul>
