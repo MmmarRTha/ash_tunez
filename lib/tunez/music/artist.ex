a2b7510a0ed8e9e618d6ee44284999308079dbeb
MmmarRTHa <m27842n@outlook.com>
2026-01-31 22:59:56 -0800

    - update: feature/chapter_2 - add previous_names attribute to artists, implement logic to track name changes in update action, add migration for previous_names column

--- lib/tunez/music/artist.ex
+++ lib/tunez/music/artist.ex
@@ -20,7 +20,22 @@ defmodule Tunez.Music.Artist do
     end
 
     update :update do
+      require_atomic? false
       accept [:name, :biography]
+
+      change fn changeset, _context ->
+               new_name = Ash.Changeset.get_attribute(changeset, :name)
+               previous_name = Ash.Changeset.get_data(changeset, :name)
+               previous_names = Ash.Changeset.get_data(changeset, :previous_names)
+
+               names =
+                 [previous_name | previous_names]
+                 |> Enum.uniq()
+                 |> Enum.reject(fn name -> name == new_name end)
+
+               Ash.Changeset.change_attribute(changeset, :previous_names, names)
+             end,
+             where: [changing(:name)]
     end
 
     destroy :destroy do
@@ -38,6 +53,10 @@ defmodule Tunez.Music.Artist do
 
     create_timestamp :inserted_at
     update_timestamp :updated_at
+
+    attribute :previous_names, {:array, :string} do
+      default []
+    end
   end
 
   relationships do
