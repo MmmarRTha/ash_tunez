9fb03c3f0e3f8436378ac25ad13ed17313e9c166
MmmarRTHa <m27842n@outlook.com>
2026-02-15 21:37:16 -0800

    - update: feature/chapter_5 - add ash authentication

--- config/test.exs
+++ config/test.exs
@@ -1,4 +1,6 @@
 import Config
+config :tunez, token_signing_secret: "5f+wwK0CMxmyfhFFBzPx+Y9XQhYW+2VX"
+config :bcrypt_elixir, log_rounds: 1
 config :ash, policies: [show_policy_breakdowns?: true], disable_async?: true
 
 # Configure your database
