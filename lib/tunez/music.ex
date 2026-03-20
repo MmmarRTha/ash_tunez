f230253d2ef088749c5926ceb2f7d84fb1727584
MmmarRTHa <m27842n@outlook.com>
2026-03-20 00:17:30 -0700

    - update: feature/chapter_9 - add unfollow artist functionality

--- lib/tunez/music.ex
+++ lib/tunez/music.ex
@@ -12,6 +12,7 @@ defmodule Tunez.Music do
       update Tunez.Music.Artist, :update_artist, :update
       destroy Tunez.Music.Artist, :destroy_artist, :destroy
       create Tunez.Music.ArtistFollower, :follow_artist, :create
+      destroy Tunez.Music.ArtistFollower, :unfollow_artist, :destroy
 
       create Tunez.Music.Album, :create_album, :create
       update Tunez.Music.Album, :update_album, :update
@@ -69,6 +70,17 @@ defmodule Tunez.Music do
       define :follow_artist do
         action :create
         args [:artist]
+
+        custom_input :artist, :struct do
+          constraints instance_of: Tunez.Music.Artist
+          transform to: :artist_id, using: & &1.id
+        end
+      end
+
+      define :unfollow_artist do
+        action :destroy
+        args [:artist]
+        get? true
 
         custom_input :artist, :struct do
           constraints instance_of: Tunez.Music.Artist
