cced7ec43fba53aecd88a1a94b4742eb6a336546
MmmarRTHa <m27842n@outlook.com>
2026-03-22 00:08:43 -0700

    - update: feature/chapter_9 - add functionality to showing and sorting artists i follow show

--- lib/tunez/music/artist.ex
+++ lib/tunez/music/artist.ex
@@ -136,7 +136,9 @@ defmodule Tunez.Music.Artist do
   calculations do
     calculate :followed_by_me,
               :boolean,
-              expr(exists(follower_relationships, follower_id == ^actor(:id)))
+              expr(exists(follower_relationships, follower_id == ^actor(:id))) do
+      public? true
+    end
   end
 
   aggregates do
@@ -149,6 +151,10 @@ defmodule Tunez.Music.Artist do
     end
 
     first :cover_image_url, :albums, :cover_image_url do
+      public? true
+    end
+
+    count :follower_count, :follower_relationships do
       public? true
     end
   end
