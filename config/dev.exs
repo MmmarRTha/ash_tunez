1bb07f737ae158fd328e2dc55c053b050f30493b
MmmarRTHa <m27842n@outlook.com>
2026-04-09 00:06:19 -0700

    - update: main - update erlang asdf version

--- config/dev.exs
+++ config/dev.exs
@@ -3,8 +3,8 @@ config :ash, policies: [show_policy_breakdowns?: true]
 
 # Configure your database
 config :tunez, Tunez.Repo,
-  username: "martha_pop",
-  password: "postgres_pop",
+  username: "backend_mn",
+  password: "fedoradb",
   hostname: "localhost",
   database: "tunez_dev",
   stacktrace: true,
